1aad0c26e4adee633c98265ad8e6cfccb597ff77
Anas-Zafar-AI <zafaranas353@gmail.com>
2026-04-06 22:24:53 +0500

    Resume analysis with groq ai

--- utils/pdf_reader.py
+++ utils/pdf_reader.py
@@ -1,1 +1,13 @@
- 
+import pdfplumber
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
+    return text
+
+# Test karo
+if __name__ == "__main__":
+    text = extract_text_from_pdf("sample_resumes/sample_resume.pdf")
+    print(text)
--- utils/scorer.py
+++ utils/scorer.py
@@ -1,1 +1,20 @@
- 
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+def analyze_resume(resume_text):
+    prompt = f"Analyze this resume: {resume_text}"
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content
+
+if __name__ == "__main__":
+    from pdf_reader import extract_text_from_pdf
+    resume_text = extract_text_from_pdf("sample_resumes/sample_resume.pdf")
+    analysis = analyze_resume(resume_text)
+    print(analysis)
